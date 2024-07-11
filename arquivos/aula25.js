35c90b25268d862b8e237640ee70b8bd2be3ac26
Victor-Viktor <69635886+Victor-Viktor@users.noreply.github.com>
2024-07-11 19:55:42 -0300

    aulas

---
+++ arquivos/aula25.js
@@ -0,0 +1,15 @@
+const soma=(v1,v2)=>v1+v2
+console.log(soma(10,5))
+
+const nome=n=>n
+console.log(nome('Victor'))
+
+const add=n=>n+10
+console.log(add(5))
+
+const somas=(v1,v2)=>{
+    let res=v1+v2
+    return res
+}
+
+console.log(somas(5,11))
